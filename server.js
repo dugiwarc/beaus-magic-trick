20d951e22bb706cddccaeec9fc4ac8572f31b8b1
dugi <contact@tusk.world>
2019-05-09 22:57:31 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,76 @@
+// Web server for node.js
+const express = require("express"),
+  // Using the web server
+  app = express(),
+  // Get the root path where the file is stored in
+  path = require("path"),
+  // Extracts body portion of incoming req stream
+  bodyParser = require("body-parser"),
+  // Database
+  mongodb = require("mongodb"),
+  MongoClient = mongodb.MongoClient,
+  // Environment variable where our db is
+
+  URI = process.env.MONGODB_URI || "mongodb://localhost:27017/magic_trick",
+  PORT = process.env.PORT || 5000,
+  DB_NAME = process.env.DB_NAME;
+
+app.get("/secret", (req, res) =>
+  res.sendFile(path.join(__dirname, "secret.html"))
+);
+
+app.post("/secret", (req, res) => {
+  MongoClient.connect(URI, { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const db = client.db(DB_NAME);
+      const collection = db.collection("names");
+      const entry = {
+        name: req.body.name.toLowerCase(),
+        card: req.body.number + "_of_" + req.body.suit
+      };
+      collection.insertOne(entry, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Inserted into database");
+        }
+      });
+      client.close();
+    }
+  });
+});
+
+app.get("/:param*", (req, res) => {
+  const name = req.url.slice(1).toLowerCase();
+
+  MongoClient.connect(URI, { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const db = client.db(DB_NAME);
+      const collection = db.collection("names");
+
+      if (name === "deleteall") {
+        collection.remove({});
+        res.send("database reset");
+      } else {
+        collection.find({ name: name }).toArray((err, result) => {
+          if (err) {
+            console.log(err);
+          } else if (result.length) {
+            const card = result[result.length - 1].card + ".png";
+            res.sendFile(path.join(__dirname + "/cards/" + card));
+          } else {
+            res.sendStatus(404);
+          }
+
+          client.close();
+        });
+      }
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
